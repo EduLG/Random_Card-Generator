c9e4e89a96c44989414d9254d77da44e1ce8ae0b
Eduardo Ladron de Guevara <eduladron@gmail.com>
2025-04-12 15:45:45 +0000

    coded HTML and js but yet to load scripting results. Then I will keep going with symbol colours and with the extras

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
 import "bootstrap";
 import "./style.css";
-
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+document.addEventListener('DOMContentLoaded', () => {
+
+  const suit = [ "&spades;", "&hearts;", "&diams;", "&clubs;"];
+  const value = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+  function randomGenerator() {
+
+    const randomSuit = suit[Math.floor(Math.random() * suit.length)];
+    const randomValue = value[Math.floor(Math.random() * value.length)];
+    
+    return {
+      suitRd: randomSuit,
+      valueRd: randomValue
+    }
+
+  }
+
+  const card = randomGenerator();
+
+  document.getElementById('upper-symbol').innerHTML = card.suitRd;
+  document.getElementById('lower-symbol').innerHTML = card.suitRd;
+  document.getElementById('card-value').innerHTML = card.valueRd;
+
+});
